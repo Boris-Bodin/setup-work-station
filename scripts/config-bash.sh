a7d6c8c04250713719592ef1714d6cee04018ae4
Boris-Bodin <boris.bodin@gmail.com>
2024-04-26 13:43:52 +0200

    Add allias disconnect

--- scripts/config-bash.sh
+++ scripts/config-bash.sh
@@ -7,7 +7,9 @@ function setPS1() {
 }
 
 function setAlias() {
-    echo "alias connect-headset=\"echo \\\"connect 88:C9:E8:C9:C1:7A\\\" | bluetoothctl\"" | cat > ~/.bash_aliases
+    echo "" | cat > ~/.bash_aliases
+    echo "alias disconnect-headset=\"echo \\\"disconnect 88:C9:E8:C9:C1:7A\\\" | bluetoothctl\"" | cat >> ~/.bash_aliases
+    echo "alias connect-headset=\"echo \\\"connect 88:C9:E8:C9:C1:7A\\\" | bluetoothctl\"" | cat >> ~/.bash_aliases
     echo "alias cg-local=\"java -jar /usr/local/bin/cg-local\"" | cat >> ~/.bash_aliases
     printlnColor "$GREEN" "TODO"
 }
